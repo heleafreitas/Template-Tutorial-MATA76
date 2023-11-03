78f5919032b2b3b3f6945763d66f6f0fa21675ee
Helen de Freitas <82972511+heleafreitas@users.noreply.github.com>
2023-11-03 15:31:27 -0300

    add project without backend

--- main.py
+++ main.py
@@ -6,108 +6,48 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    select = 'SELECT * FROM activities'
-    lista = listarTarefas(select=select)
-    return render_template('home.html', data=lista)
+    pass
 
 @app.route('/filtrar', methods=['POST'])
 def filtrar():
-    filtro = request.form['filtro']
-    if filtro == '':
-        return redirect(url_for('home'))
-    select = f'SELECT * FROM activities WHERE date = "{filtro}"'
-    lista = listarTarefas(select=select)
-    return render_template('home.html', data=lista)
+    pass
 
 @app.route('/iniciar_tarefa/<string:id>', methods=['POST','GET'])
 def iniciar_tarefa(id):
-    identificador = int(id)
-    resposta = db.select_from_table(custom_query=f"SELECT status FROM activities WHERE id = {identificador}")
-    if resposta[0][0] == 'não iniciada':
-        db.execute_query(f"UPDATE activities SET status='iniciada' WHERE id = {identificador}")
-    return redirect(url_for('home'))
+    pass
 
 @app.route('/finalizar_tarefa/<string:id>', methods=['POST','GET'])
 def finalizar_tarefa(id):
-    identificador = int(id)
-    resposta = db.select_from_table(custom_query=f"SELECT status FROM activities WHERE id = {identificador}")
-    if resposta[0][0] == 'iniciada':
-        db.execute_query(f"UPDATE activities SET status='finalizada' WHERE id = {identificador}")
-    return redirect(url_for('home'))
+    pass
 
 @app.route('/deletar_tarefa/<string:id>', methods=['POST','GET'])
 def deletar_tarefa(id):
-    identificador = int(id)
-    db.execute_query(f"DELETE FROM activities WHERE id = {identificador}")
-    return redirect(url_for('home'))
+    pass
 
 @app.route('/editar_tarefa/<string:id>', methods=['POST','GET'])
 def editar_tarefa(id):
-    identificador = int(id)
-    resposta = db.select_from_table(custom_query=f"SELECT id, name, description, date FROM activities WHERE id = {identificador}")
-    id = resposta[0][0]
-    nome = resposta[0][1]
-    descricao = resposta[0][2]
-    data = resposta[0][3]
-    return render_template('update.html', nome=nome, descricao=descricao, data=data, id=id)
+    pass
 
 @app.route('/atualizar/<string:id>', methods=['POST','GET'])
 def atualizar(id):
-    descricao = request.form['descricao']
-    nome = request.form['nome']
-    data = request.form['data']
-
-    if descricao != '':
-         db.execute_query(f"UPDATE activities SET description='{descricao}' WHERE id = {int(id)}")
-    if nome != '':
-         db.execute_query(f"UPDATE activities SET name='{nome}' WHERE id = {int(id)}")
-    if data != '':
-         db.execute_query(f"UPDATE activities SET date='{data}' WHERE id = {int(id)}")
-
-    return redirect(url_for('home'))
+    pass
 
 @app.route('/irParaAdicionar', methods=['POST'])
 def irParaAdicionar():
-    return redirect(url_for('registrar_atividade'))
+    pass
 
 @app.route('/register')
 def registrar_atividade():
-    return render_template('register.html')
+   pass
 
 @app.route('/adicionar_tarefa', methods=['POST'])
 def adicionar_tarefa():
-    nome = request.form['nome']
-    descricao = request.form['descricao']
-    data = request.form['data']
-    inicio = 'null'
-    fim = 'null'
-    status = 'não iniciada'
-    values = (nome, descricao, data, inicio, fim, status)
-
-    db.execute_query(query=db.QUERY_INSERT_TABLE_ACTIVITIES, data=values)
+    pass
     
     return redirect(url_for('home'))
 
 def listarTarefas(select):
-    resposta = db.select_from_table(custom_query=select)
-    lista = []
-    for i in range(len(resposta)):
-        id = resposta[i][0]
-        tarefa = resposta[i][2]
-        data = resposta[i][3]
-        status = resposta[i][6]
-
-        classe = 'categoryRed'
-
-        if status == 'finalizada':
-            classe = 'categoryGreen'
-        elif status == 'iniciada':
-            classe = 'categoryYellow'
-
-        objeto = {'tarefa': tarefa, 'id': id, 'dia': data, 'classe': classe}
-        lista.append(objeto)
-
-    return lista
+    pass
 
 app.run(debug=True)
 
